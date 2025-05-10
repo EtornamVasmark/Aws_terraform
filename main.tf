3ec33748ad716e927805daf3318fdf2bfa794ffc
Etornam Vasmark-Voegborlo <etornamvasmark-voegborlo@Etornams-MacBook-Air.local>
2025-05-10 13:44:24 +0200

    cool CAPSTONE

--- main.tf
+++ main.tf
@@ -11,15 +11,3 @@ terraform {
 resource "aws_vpc" "wordpress-vpc" {
   cidr_block = "10.0.0.0/24"
 }
-
-# Creating Subnets
-# Create Public Subnet 1 in the VPC
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.aws_capstone_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1" # Replace with your desired AZ
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "PublicSubnet1"
-  }
-}
