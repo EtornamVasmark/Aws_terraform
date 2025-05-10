6ab03491012dce88f5b9d9768d75d737600d55cf
Etornam Vasmark-Voegborlo <etornamvasmark-voegborlo@Etornams-MacBook-Air.local>
2025-05-10 13:34:05 +0200

    cool CAPSTONE

--- main.tf
+++ main.tf
@@ -7,7 +7,26 @@ terraform {
   }
 }
 
+# Add your variable declarations here
+
+variable "aws_availability_zone_a" {
+  description = "The availability zone for the public subnet 1"
+  type        = string
+}
+
 # Creating VPC
 resource "aws_vpc" "wordpress-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/24"
+}
+
+# Creating Subnets
+# Create Public Subnet 1 in the VPC
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.aws_capstone_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = var.aws_availability_zone_a
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet1"
+  }
 }
