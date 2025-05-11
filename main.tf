9276b6fa05906a14094f7b8641cc2429ac1cf7b3
Etornam Vasmark-Voegborlo <etornamvasmark-voegborlo@Etornams-MacBook-Air.local>
2025-05-11 21:53:25 +0200

    cool CAPSTONE

--- main.tf
+++ main.tf
@@ -65,4 +65,35 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+# -------------------------------------------
+# SECURITY GROUP
+# -------------------------------------------
+resource "aws_security_group" "web_sg" {
+  name        = "wordpress-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "wordpress-web-sg" }
+}
 
